4e55ea7d8da27b5c34ea6729694dcf67b70d66f3
Ty <tyycali@gmail.com>
2021-05-12 12:30:51 -0700

    genesis

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OasisComponent } from './oasis/oasis.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'next',component:OasisComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/oasis/oasis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oasis',
+  templateUrl: './oasis.component.html',
+  styleUrls: ['./oasis.component.scss']
+})
+export class OasisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
