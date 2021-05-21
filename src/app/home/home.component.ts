05c6ed8d22e2de47cabf52c677bf046601a6c430
Ty <tyycali@gmail.com>
2021-05-21 13:49:37 -0700

    deploy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'gitMerge MATE'
+  title = 'Ultimate Trakr'
 
   constructor() { }
 
